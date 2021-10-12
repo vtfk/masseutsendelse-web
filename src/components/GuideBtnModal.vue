7dbfdd02c571673c10b2f8748855529259430a2d
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-10-12 15:59:21 +0200

    Påbegynt funksjonalitet på modalknapp

--- src/components/GuideBtnModal.vue
+++ src/components/GuideBtnModal.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <VTFKButton>Se mer</VTFKButton>
+    <VTFKButton :passedProps="{onClick: onClick}">Se mer</VTFKButton>
+    <VTFKModal title="Guide" :open="isShowModal" :onDismiss="() => { this.isShowModal = false }" style="z-index: 1001;">
+      <VTFKModalBody>
+        <div class="header-part typography heading-one">Bruk av siden</div>
+
+        Her er det noe teskt
+      </VTFKModalBody>
+    </VTFKModal>
   </div>
 </template>
 
 <script>
 // VTFK komponenter
-import { Button } from '@vtfk/components'
+import { Button, Modal, ModalBody } from '@vtfk/components'
 
 export default {
   name: 'GuideBtnModal',
   components: {
-    'VTFKButton': Button
+    'VTFKButton': Button,
+    'VTFKModal': Modal,
+    'VTFKModalBody': ModalBody
   },
   data() {
     return {
-      
+      isShowModal: false
+    }
+  },
+  methods: {
+    onClick() {
+      this.isShowModal = !this.isShowModal;
     }
   }
 }
