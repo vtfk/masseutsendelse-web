06959f4d3a3d91f400cad134d1e440ad35111c96
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-10-12 11:01:36 +0200

    Fikset litt på layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
   <div id="app">
-    <header class="container">
+    <header>
+      <div class="container">
       <Header />
+      </div>
     </header>
-    <div class="container">
-      <!-- Informasjonstekst -->
-      <div>
-        <div class="typography heading-two header-title">Header</div>
-        <div class="typography paragraph">
-          Her blir det
-          noe
-          tekst
+    <main style="height: 100%">
+      <div class="container">
+        <!-- Informasjonstekst -->
+        <div style="display: flex; align-items: center; flex-direction: column;">
+          <div class="typography heading-two header-title">Masseutsendelse</div>
+          <p class="typography paragraph header-description" style="margin-top: 1rem;">
+            Ett verktøy utviklet for Samferdesel og mobilitets sektoren.<br/>
+            Verktøyet lar deg laste opp en polygon fil, gjøre oppslag i Matrikkelen og varsle alle eiere som befinner seg innenfor polygonet.
+          </p>
+          <GuideBtnModal class="centered" style="margin-top: 1rem;"/>
         </div>
-        <GuideBtnModal />
+        
+        <!-- Upload felt -->
+        <div v-if="!hasLoadedFile">
+          <UploadField v-on:uploaded="() => hasLoadedFile = true"/>
+        </div>
+        <div v-else>
+          <!-- Kart komponent -->
+          <Map />
+          <!-- Cards som viser stats om informasjonen -->
+          <!-- Angreknapp -->
+          <VTFKButton>Angre</VTFKButton>
+        </div>
       </div>
-      <!-- Upload felt -->
-      <div v-if="!hasLoadedFile">
-        <UploadField v-on:uploaded="() => hasLoadedFile = true"/>
-      </div>
-      <div v-else>
-        <!-- Kart komponent -->
-        <Map />
-        <!-- Cards som viser stats om informasjonen -->
-        <!-- Angreknapp -->
-        <VTFKButton>Angre</VTFKButton>
-      </div>
-    </div>
+    </main>
   </div>
 </template>
 
@@ -74,12 +78,15 @@ export default {
   }
 
   .container {
-    display: flex;
-    justify-content: center;
+
     padding-top: 4rem;
     padding-left: 1rem;
     padding-right: 1rem;
     max-width: 1100px;
+    margin: 0 auto;
+  }
+
+  .centered {
     margin: 0 auto;
   }
 
