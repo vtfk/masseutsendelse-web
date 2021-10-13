5802c23a44edb5d3614356e4325dbf83bd1b58da
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-10-13 12:24:07 +0200

    test

--- src/components/UploadField.vue
+++ src/components/UploadField.vue
@@ -4,7 +4,6 @@
       <form enctype="multipart/form-data" novalidate v-if="isInitial">
         <h2>Last opp polygonet</h2>
         <div class="dropbox">
-<<<<<<< HEAD
           <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files);"
              class="input-file">
             <p v-if="!isInitial">
@@ -13,19 +12,11 @@
             <p v-if="isSaving">
               Laster opp {{ uploadFieldName }} fil...
             </p>
-=======
-          <input type="file" 
-          class="input-file">
-            <p v-if="isInitial">
-              Dra filen og slipp den i feltet<br> eller trykk i feltet for å laste opp
-            </p>
->>>>>>> b8372bab83b32927b4b4f37d9b21ef707cbdae3c
         </div>
           <div v-if="isSuccess">
           <h2>Filen er lastet opp</h2>
           <p>
           <a href="javascript:void(0)" @click="reset()">Last opp en ny fil.</a>
-<<<<<<< HEAD
         </p>
         <ul class="list-unstyled">
             <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">
@@ -39,10 +30,6 @@
         </p>
         <pre>{{ uploadError }}</pre>
       </div>
-=======
-          </p>
-        </div>
->>>>>>> b8372bab83b32927b4b4f37d9b21ef707cbdae3c
       </form>
 </div>
 </template>
@@ -55,11 +42,6 @@ export default {
   name: 'UploadField',
   data() {
     return {
-<<<<<<< HEAD
-=======
-      uploadedFiles: [],
-      currentStatus: null,
->>>>>>> b8372bab83b32927b4b4f37d9b21ef707cbdae3c
     }
   },
   computed: {
