d78af0513aeae40fc5a4fdb267c96bd9b1f93f5f
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-10-13 08:05:39 +0200

    endringer

--- src/components/UploadField.vue
+++ src/components/UploadField.vue
@@ -4,13 +4,13 @@
       <form enctype="multipart/form-data" novalidate v-if="!isInitial || isSaving">
         <h2>Last opp polygonet</h2>
         <div class="dropbox">
-          <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
-            accept="image/*" class="input-file">
+          <input type="file" :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files);"
+             class="input-file">
             <p v-if="!isInitial">
               Dra filen og slipp den i feltet<br> eller trykk i feltet for å laste opp
             </p>
             <p v-if="isSaving">
-              Laster opp {{ fileCount }} fil...
+              Laster opp {{ uploadFieldName }} fil...
             </p>
         </div>
         <div v-if="isSuccess">
@@ -19,16 +19,14 @@
           <a href="javascript:void(0)" @click="reset()">Last opp en ny fil.</a>
         </p>
         <ul class="list-unstyled">
-          <li v-for="item in uploadedFiles" :key="item.url">
             <img :src="item.url" class="img-responsive img-thumbnail" :alt="item.originalName">
-          </li>
         </ul>
       </div>
       <!--FAILED-->
       <div v-if="isFailed">
         <h2>Obs noe gikk galt :(</h2>
         <p>
-          <a href="javascript:void(0)" @click="reset()">Try again</a>
+          <a href="javascript:void(0)" @click="reset()">Prøv igjen</a>
         </p>
         <pre>{{ uploadError }}</pre>
       </div>
@@ -43,7 +41,6 @@ export default {
   name: 'UploadField',
   data() {
     return {
-      uploadedFiles: [],
     }
   }
 }
@@ -51,18 +48,18 @@ export default {
 
 <style scoped>
 .dropbox {
-    outline: 2px dashed grey; /* the dash box */
+    outline: 2px dashed grey; /* dash box */
     outline-offset: -10px;
     background: #D1EAE9;
     color: dimgray;
     padding: 10px 10px;
-    min-height: 200px; /* minimum height */
+    min-height: 200px; 
     position: relative;
     cursor: pointer;
   }
 
   .input-file {
-    opacity: 0; /* invisible but it's there! */
+    opacity: 0; 
     width: 100%;
     height: 200px;
     position: absolute;
@@ -70,7 +67,7 @@ export default {
   }
 
   .dropbox:hover {
-    background: lightblue; /* when mouse over to the drop zone, change color */
+    background: lightblue; 
   }
 
   .dropbox p {
