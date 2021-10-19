166bad2f811dfb5168c7abd7f795dad58d8e5145
Karl-Einar Bergstrøm <karl-einarb@outlook.com>
2021-10-19 13:15:42 +0200

    Installert Vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify, { VDataTable, VDataTableHeader } from 'vuetify/lib'
+
+Vue.use(Vuetify, {
+  components: {
+    VDataTable,
+    VDataTableHeader
+  }
+});
+
+export default new Vuetify({
+});
