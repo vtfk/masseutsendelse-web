f0454696ce77cec7f2b4e0c87bce6754c8468ab2
Robin Rennesund Ellingsen <46957821+robinElli@users.noreply.github.com>
2021-10-22 13:25:51 +0200

    v-select for status  m/ styling

--- src/views/Utsendelser.vue
+++ src/views/Utsendelser.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h2 class="typography heading-two" style="margin: 2rem;">Utsendelser</h2>
-    <v-card>
+    <v-card style="padding-bottom:2rem;">
       <v-card-title class="typography heading-three">
         Prosjekter
         <v-spacer/>
@@ -20,7 +20,7 @@
         :items-per-page="5"
         fixed-header
         class="elevation-1"
-        style="margin: 2rem"
+        style="margin: 2rem;"
         :search="search"
         :loading="loading"
         loading-text="Laster data fra databasen "
@@ -38,6 +38,7 @@
             medium
             style="padding-right:0.2rem;"
             @click="editItem(item)"
+            v-on:click="test"
           >
             mdi-pencil
           </v-icon>
@@ -57,9 +58,10 @@
       </template>
       </v-data-table>
     </v-card>
-    <!-- MODALER -->
+    <!-- MODALER/DIALOGER -->
+    <!-- Edit dialog -->
     <v-dialog
-    v-model="dialog"
+    v-model="dialogEdit"
     width="50%"
     >
     <v-card>
@@ -67,25 +69,60 @@
         Sett status
       </v-card-title>
         <v-card-text>
-          <v-text-field
-            label="Status"
-          ></v-text-field>
-      </v-card-text>
-        <v-card-actions style="display:flex; gap:1rem;">
+          <v-select
+          v-model="select"
+          :hint="`Prosjektets status vil bli satt til ${select.status_valg}`"
+          :items="items"
+          item-text="status_valg"
+          item-value="status_value"
+          label="Sett status for prosjektet"
+          :menu-props="{ bottom: true, offsetY: true }"
+          chips
+          persistent-hint
+          hide-selected
+          outlined
+          rounded
+          return-object
+          single-line
+        >
+          <template #selection="{item}">
+          <v-chip
+           :color="getColor(item.status_value)"
+          >
+            {{ item.status_value }}
+          </v-chip>
+        </template>
+        </v-select>
+        </v-card-text>
+        <v-card-actions style="display:flex; gap:1rem;" class="centerbtn">
           <VTFKButton 
-                type='secondary' size='small' style="padding-bottom: 1rem;"
-                :passedProps="{ onClick: () => viewDocument() }"
-                >Lagre
-                </VTFKButton>
-                <VTFKButton 
-                type='secondary' size='small' style="padding-bottom: 1rem;"
-                :passedProps="{ onClick: () => viewDocument() }"
-                >Avbryt
-                </VTFKButton>
+            type='secondary' size='small' style="padding-bottom: 1rem;"
+            :passedProps="{ onClick: () => [saveEdit(), clear()] }"
+            v-on:click="clear"
+            >Lagre
+          </VTFKButton>
+          <VTFKButton 
+            type='secondary' size='small' style="padding-bottom: 1rem;"
+            :passedProps="{ onClick: () => [dialogEdit = false, clear()] }"
+            >Avbryt
+          </VTFKButton>
         </v-card-actions>
     </v-card>
     </v-dialog>
+    <!-- Map dialog -->
+    <!-- Doc dialog -->
 
+    <!-- Alerts -->
+    <v-alert 
+    :value="alert_success" 
+    type="success"
+    color="#91B99F"
+    width="50%"
+    rounded="xl"
+    transition="slide-y-transition"
+    class="centered">
+      Statusen er lagret.
+    </v-alert>
   </div>
 </template>
 
@@ -104,6 +141,7 @@ import { Button } from '@vtfk/components'
         dialogEdit: false,
         dialogDelete:false,
         loading:false,
+        alert_success: false,
         headers: [
           {
             text: 'Prosjekt',
@@ -119,53 +157,61 @@ import { Button } from '@vtfk/components'
           {text: 'Filnavn', value: 'filnavn'},
           {text: 'Handlinger', value: 'handlinger', sortable:false}
         ],
-          prosjekter: [
-            {
-              navn:'Ulefoss',
-              prosjektnr:'1',
-              dato:'01.02.2021',
-              status:'Godkjent',
-              oppretshaver:'Per',
-              behandletav: 'Lars',
-              filnavn:'Ulefoss Polygon.dfx',
-            },
-            {
-              navn:'Gvarv',
-              prosjektnr:'2',
-              dato:'02.03.2021',
-              status:'Godkjent',
-              oppretshaver:'Per',
-              behandletav: 'Lars',
-              filnavn:'Gvarv Polygon.dfx',
-            },
-            {
-              navn:'Gvarv',
-              prosjektnr:'2',
-              dato:'01.03.2021',
-              status:'Ikke Godkjent',
-              oppretshaver:'Per',
-              behandletav: 'Lars',
-              filnavn:'Gvarv Polygon.dfx',
-            },
-            {
-              navn:'Bø i Telemark',
-              prosjektnr:'3',
-              dato:'01.01.2020',
-              status:'Sendt',
-              oppretshaver:'Per',
-              behandletav: 'Lars',
-              filnavn:'Bø Polygon.dfx',
-            },
-            {
-              navn:'Skien',
-              prosjektnr:'4',
-              dato:'05.02.2021',
-              status:'Til Behandling',
-              oppretshaver:'Lars',
-              behandletav: 'Per',
-              filnavn:'Skien Polygon.dfx',
-            },
-          ]
+        prosjekter: [
+          {
+            navn:'Ulefoss',
+            prosjektnr:'1',
+            dato:'01.02.2021',
+            status:'Godkjent',
+            oppretshaver:'Per',
+            behandletav: 'Lars',
+            filnavn:'Ulefoss Polygon.dfx',
+          },
+          {
+            navn:'Gvarv',
+            prosjektnr:'2',
+            dato:'02.03.2021',
+            status:'Godkjent',
+            oppretshaver:'Per',
+            behandletav: 'Lars',
+            filnavn:'Gvarv Polygon.dfx',
+          },
+          {
+            navn:'Gvarv',
+            prosjektnr:'2',
+            dato:'01.03.2021',
+            status:'Ikke Godkjent',
+            oppretshaver:'Per',
+            behandletav: 'Lars',
+            filnavn:'Gvarv Polygon.dfx',
+          },
+          {
+            navn:'Bø i Telemark',
+            prosjektnr:'3',
+            dato:'01.01.2020',
+            status:'Sendt',
+            oppretshaver:'Per',
+            behandletav: 'Lars',
+            filnavn:'Bø Polygon.dfx',
+          },
+          {
+            navn:'Skien',
+            prosjektnr:'4',
+            dato:'05.02.2021',
+            status:'Til Behandling',
+            oppretshaver:'Lars',
+            behandletav: 'Per',
+            filnavn:'Skien Polygon.dfx',
+          },
+        ],
+        select: {status_valg: '', status_value: ''},
+        items: [
+          { status_valg: 'Godkjent', status_value: 'Godkjent'},
+          { status_valg: 'Ikke Godkjent', status_value: 'Ikke Godkjent'},
+          { status_valg: 'Til Behandling', status_value: 'Til Behandling'},
+          { status_valg: 'Sendt', status_value: 'Sendt'},
+        ],
+        fetchStatus: '',
       }
     },
     methods: {
@@ -179,19 +225,65 @@ import { Button } from '@vtfk/components'
       editItem (item) {
         this.editedIndex = this.prosjekter.indexOf(item)
         this.editedItem = Object.assign({}, item)
-        this.dialog = true
+        this.dialogEdit = true
+        this.fetchStatus = `${this.editedItem.status}`
+        this.select = this.fetchStatus
+        // console.log(this.editedItem.status)
+        // console.log(this.editedIndex)
+        // console.log(this.editedItem)
       },
       openMap(){
-        
+        // TODO
+        // En funksjon som åpner kartet til polygonet til det valgte prosjektet
       },
       openLetter(){
         console.log("Hei")
+      },
+      saveEdit() {
+        this.dialogEdit = false
+        this.alert_success = true
+        this.hide_alert();
+        // console.log(this.editedItem.status)
+        // this.editedItem.status = this.select.status_value
+        // console.log(this.editedItem.status)
+        // console.log(this.editedItem)
+        // console.log(this.select.status_value)
+      },
+      selectedItem() {
+        return this.editedItem.status
+      },
+      clear(){
+          this.select = this.clear
+      },
+      test(){
+        this.editItem.status = this.select
+        // console.log(this.editedItem.status)
+        // console.log(this.select.status_value)
+      },
+      hide_alert: function () {
+        window.setInterval(() => {
+          this.alert_success = false
+        }, 3000)    
       }
     },
   }
 </script>
 
 <style scoped>
-
-
+.wrapper {
+  box-shadow: 0px 1px 5px 1px #888888;
+}
+.centerbtn {
+  width: 100%;
+  text-align:center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.center-alert {
+  text-align:center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
